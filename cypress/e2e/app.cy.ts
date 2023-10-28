5b2c67dce5270a46968f0eebea1f1cd4d3af82b3
Alex-Programer <alex.lichangnan@gmail.com>
2023-10-29 02:23:52 +0800

    test: ui

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,14 +1,24 @@
-describe('e2e test', () => {
-  beforeEach(() => {
-    cy.visit('iframe.html?id=component-app--primary&viewMode=story');
+beforeEach(() => {
+  cy.visit('iframe.html?id=component-app--primary&viewMode=story');
+  cy.get('.block-subtitle').should('be.visible')
+})
+
+describe('ui test', () => {
+  it('default block ui', () => {
+    cy.expectStyle('.block-subtitle', {
+      width: '400px',
+      height: '100px',
+      backgroundColor: 'rgb(0, 0, 0)'
+    });
   })
 
-  it('startup', () => {
-    cy.get('.block-subtitle').should('be.visible');
-    cy.get('body').type('{shift}', { release: false });
-    cy.get('body').type('{r}', { release: false });
+  it('set mode', () => {
+    cy.get('body').type('{shift},{r}', { release: false });
+    cy.get('body').type('{shift},{r}', { release: true });
 
-    cy.get('body').type('{shift}', { release: true });
-    cy.get('body').type('{r}', { release: true });
+    cy.get('.ant-drawer-close').should('be.visible');
+    cy.get('.ant-drawer-title').should('have.text', 'Set Mode');
+    cy.get('.ant-btn-default > span').should('have.text', 'Reset');
+    cy.get('.ant-btn-primary > span').should('have.text', '+ Add Record');
   })
 })
